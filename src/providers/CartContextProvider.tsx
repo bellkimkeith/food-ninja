1ca4c160c75fdabce50d4c5f9ebe836d9db2fb34
bellkimkeith <157334752+bellkimkeith@users.noreply.github.com>
2024-03-03 13:15:59 +0800

    Add to cart quantity and duplicate fix
    Used expo crypto to generate a more unique id with randomUUID

--- src/providers/CartContextProvider.tsx
+++ src/providers/CartContextProvider.tsx
@@ -1,10 +1,11 @@
 import { CartItem, Product } from "@/utils/types";
 import { PropsWithChildren, createContext, useContext, useState } from "react";
+import { randomUUID } from "expo-crypto";
 
 type CartType = {
   cartItems: CartItem[];
   addCartItem: (product: Product, quantity: CartItem["quantity"]) => void;
-  updateQuantity: () => void;
+  updateQuantity: (itemId: string, amount: number) => void;
 };
 
 const CartContext = createContext<CartType>({
@@ -17,8 +18,15 @@ const CartContextProvider = ({ children }: PropsWithChildren) => {
   const [cartItems, setCartItems] = useState<CartItem[]>([]);
 
   const addCartItem = (product: Product, quantity: CartItem["quantity"]) => {
+    const cartItemExists = cartItems.find((item) => item.product === product);
+
+    if (cartItemExists) {
+      updateQuantity(product.id.toString(), quantity);
+      return;
+    }
+
     const newCartItem: CartItem = {
-      id: new Date().toISOString(),
+      id: randomUUID(),
       product,
       product_id: product.id,
       quantity,
@@ -27,7 +35,17 @@ const CartContextProvider = ({ children }: PropsWithChildren) => {
     setCartItems([newCartItem, ...cartItems]);
   };
 
-  const updateQuantity = () => {};
+  const updateQuantity = (itemId: string, amount: number) => {
+    const updatedCartItems = cartItems
+      .map((item) =>
+        item.product.id.toString() !== itemId
+          ? item
+          : { ...item, quantity: item.quantity + amount }
+      )
+      .filter((item) => item.quantity > 0);
+
+    setCartItems(updatedCartItems);
+  };
 
   return (
     <CartContext.Provider value={{ cartItems, addCartItem, updateQuantity }}>
