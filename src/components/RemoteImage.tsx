e8bbd310360aa5eab63c518d2a0571d28a3bb14f
bellkimkeith <157334752+bellkimkeith@users.noreply.github.com>
2024-03-30 23:12:02 +0800

    update for pager package and remote image loading

--- src/components/RemoteImage.tsx
+++ src/components/RemoteImage.tsx
@@ -1,4 +1,4 @@
-import { Image } from "react-native";
+import { ActivityIndicator, Image } from "react-native";
 import React, { ComponentProps } from "react";
 import { useProductImage } from "@/api/products";
 import { useProfileImage } from "@/api/profiles";
@@ -17,28 +17,36 @@ const RemoteImage = ({
 }: RemoteImageProps) => {
   let data: any = null;
   let error: Error | null = null;
+  let loading: boolean = true;
 
   switch (imageType) {
     case "product":
-      const { data: productData, error: productError } = useProductImage(
-        path ? path : fallback
-      );
+      const {
+        data: productData,
+        error: productError,
+        isLoading: productLoading,
+      } = useProductImage(path ? path : fallback);
       data = productData;
       error = productError;
+      loading = productLoading;
       break;
 
     case "profile":
-      const { data: profileData, error: profileError } = useProfileImage(
-        path ? path : fallback
-      );
+      const {
+        data: profileData,
+        error: profileError,
+        isLoading: profileLoading,
+      } = useProfileImage(path ? path : fallback);
       data = profileData;
       error = profileError;
-
+      loading = profileLoading;
       break;
 
     default:
       break;
   }
+
+  if (loading) return <ActivityIndicator />;
 
   if (error || !data)
     return <Image source={{ uri: fallback }} {...imageProps} />;
