4ff4a0e3433cfd3cd1ba64b92146e07cbde1b357
bellkimkeith <157334752+bellkimkeith@users.noreply.github.com>
2024-04-15 11:32:07 +0800

    hide change image button

--- src/app/(user-tabs)/profile.tsx
+++ src/app/(user-tabs)/profile.tsx
@@ -184,13 +184,16 @@ const ProfileScreen = () => {
           resizeMode="contain"
         />
       )}
-      <Text
-        style={[styles.textButton, !isEditingProfile && { color: "#ccc" }]}
-        onPress={pickImage}
-        disabled={!isEditingProfile}
-      >
-        Change Image
-      </Text>
+      {isEditingProfile && (
+        <Text
+          style={[styles.textButton, !isEditingProfile && { color: "#ccc" }]}
+          onPress={pickImage}
+          disabled={!isEditingProfile}
+        >
+          Change Image
+        </Text>
+      )}
+
       <TextInput
         style={[
           styles.input,
@@ -255,6 +258,7 @@ const styles = StyleSheet.create({
     width: "50%",
     aspectRatio: 1,
     borderRadius: 12,
+    marginBottom: 10,
   },
   textButton: {
     fontSize: 18,
